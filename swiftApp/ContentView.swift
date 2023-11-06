9f18b234cf3b559285c5b8ea2c90aeba32d62074
Faheem Danish <danish42005@gmail.com>
2023-11-06 09:44:37 +0100

    Initial Commit

---
+++ swiftApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftApp
+//
+//  Created by Faheem Danish on 2023-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftApp/swiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftAppApp.swift
+//  swiftApp
+//
+//  Created by Faheem Danish on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct swiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
